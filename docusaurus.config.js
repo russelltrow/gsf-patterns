4d5e96848ca8e3d192f6b3bb4442c1c5e042b5ff
Russell Trow <russell@greensoftware.foundation>
2026-01-15 16:49:20 +0000

    Update site URL in docusaurus.config.js
    Signed-off-by: Russell Trow <russell@greensoftware.foundation>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   tagline:
     "An online open-source database of green software patterns reviewed and curated by the Green Software Foundation",
   // Change to site url
-  url: "https://patterns.greensoftware.foundation/",
+  url: "https://russelltrow.github.io/gsf-patterns/",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
