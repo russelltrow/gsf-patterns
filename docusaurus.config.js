7b92df794a2c4bc49bd40a354a79bcbd233f19b5
Russell Trow <russell@greensoftware.foundation>
2026-01-15 17:00:03 +0000

    Change baseUrl format in docusaurus.config.js
    Updated base URL format for Docusaurus configuration.
    
    Signed-off-by: Russell Trow <russell@greensoftware.foundation>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
     "An online open-source database of green software patterns reviewed and curated by the Green Software Foundation",
   // Change to site url
   url: "https://russelltrow.github.io/",
-  baseUrl: "/gsf-patterns",
+  baseUrl: "gsf-patterns/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
